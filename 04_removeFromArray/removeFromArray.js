eecfb660f25f20ec0c06feab841453f47109aa57
ColtMoons <coltmoonsac@gmail.com>
2022-07-29 23:04:05 -0500

    Add solution to 04 JavaScript

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
+    let args = [...arguments];
+    let array = args[0];
 
+    args.shift();
+
+    let result = array.filter((data)=>{
+        for(const arg of args){
+            if(data === arg){
+                return false
+            }
+        }
+        return true;
+    })
+    return result;
 };
 
 // Do not edit below this line
