c7cbde59a1bfd120130a8a17db7e0caa6498dade
ColtMoons <coltmoonsac@gmail.com>
2022-07-29 23:31:00 -0500

    Add solution to 05 JavaScript SumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(typeof(a) === "number" && typeof(b) === "number" && a >= 0 && b >= 0){
+        let sum = 0;
+        let max = a > b ? a : b
+        let min = a < b ? a : b
+        
+        for(let i = min; i <= max; i++){
+            sum  += i
+        }
+        return sum;
+    }else{
+        return "ERROR"
+    }
+}
 
-};
-
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
