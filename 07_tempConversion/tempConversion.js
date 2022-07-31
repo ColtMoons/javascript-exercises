ebfce6602d0849dadfa88026ad6f3ce6376bd088
ColtMoons <coltmoonsac@gmail.com>
2022-07-30 21:51:38 -0500

    Add solution to Temp Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  return Math.round((temperature - 32) * (5 / 9)* 10) / 10;
 };
 
-const ctof = function() {
+console.log(ftoc(32))
 
+const ctof = function(temperature) {
+  return Math.round((temperature * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
